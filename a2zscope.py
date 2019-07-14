127da4742079ef1bab8918d15852661a43237a81
Henry Park <hanry.park3@gmail.com>
2019-07-14 02:56:13 -0700

    Beautify syntax

--- a2zscope.py
+++ a2zscope.py
@@ -1,21 +1,27 @@
 import boto3
 import base64
 import json
+import validators
 
 def lambda_handler(event, context):
-    detected_text = []
-    photo = event['photo'] 
     client=boto3.client('rekognition', region_name='us-west-2')
-    response=client.detect_text(Image={'Bytes': base64.b64decode(photo)})
-    texDetections=response['TextDetections']
+
+    encodedPhoto = event['photo'] 
+    decodedPhoto = {'Bytes': base64.b64decode(encodedPhoto)}
+
+    response=client.detect_text(Image=decodedPhoto)
+    textDetections=response['TextDetections']
+
+    detected_text = []
     for text in textDetections:
         detected_text.append(text['DetectedText'])
 
     urls = set()
     for url in detected_text:
-        url.split(' ')
-        if validators.url(url) == True:
+        url.split()
+        if validators.url(url):
             urls.add(url)
-return { 
-    urls : list(urls)
-}
+
+    return {
+        urls : list(urls)
+    }
