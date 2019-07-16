d0eb49d6969c9422905a5af9b6c0ef847745626f
Henry Park <hanry.park3@gmail.com>
2019-07-15 20:49:54 -0700

    Add static value and remove unnecessary files

--- a2zscope.py
+++ a2zscope.py
@@ -3,14 +3,18 @@ import base64
 import json
 import validators
 
+AWS_SERVICE = 'rekognition'
+AWS_REGION_NAME = 'us-west-2'
+AWS_FEATURE_TYPE = 'TextDetections'
+
 def lambda_handler(event, context):
-    client=boto3.client('rekognition', region_name='us-west-2')
+    client=boto3.client(AWS_SERVICE, AWS_REGION_NAME)
 
     encodedPhoto = event['photo'] 
     decodedPhoto = {'Bytes': base64.b64decode(encodedPhoto)}
 
     response=client.detect_text(Image=decodedPhoto)
-    textDetections=response['TextDetections']
+    textDetections=response[AWS_FEATURE_TYPE]
 
     detected_text = []
     for text in textDetections:
